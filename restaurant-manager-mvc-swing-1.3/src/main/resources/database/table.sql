e60a629cde350c288fb1b1f02be8ad6df59048bf
duong vu cong tuan <vucongtuanduong2409@gmail.com>
2024-09-27 22:48:50 +0700

    update

--- restaurant-manager-mvc-swing-1.3/src/main/resources/database/table.sql
+++ restaurant-manager-mvc-swing-1.3/src/main/resources/database/table.sql
@@ -1,84 +1,112 @@
-CREATE TABLE IF NOT EXISTS `Customer` (
-    `id` int NOT NULL AUTO_INCREMENT,
+DROP DATABASE IF EXISTS milktea;
+CREATE DATABASE IF NOT EXISTS milktea;
+USE milktea;
+CREATE TABLE IF NOT EXISTS `customer` (
+    `customerId` int NOT NULL AUTO_INCREMENT,
     `phoneNumber` varchar(20) NOT NULL,
     `name` varchar(50) NULL,
     `address` varchar(250) NULL,
-    `birthday` TIMESTAMP NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`customerId`)
 );
-CREATE TABLE IF NOT EXISTS `Table` (
-    `id` int NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE IF NOT EXISTS `table` (
+    `tableId` int NOT NULL AUTO_INCREMENT,
     `name` varchar(45) NOT NULL,
     `status` varchar(45) NOT NULL DEFAULT 'free' COMMENT 'free-Trống\nserving-Đang phục vụ\nreserving-Đặt trước',
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`tableId`)
 );
-CREATE TABLE IF NOT EXISTS `Employee` (
-    `id` int NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE IF NOT EXISTS `employee` (
+    `employeeId` int NOT NULL AUTO_INCREMENT,
     `username` varchar(50) UNIQUE NOT NULL,
     `password` varchar(50) NOT NULL,
     `name` varchar(50) NOT NULL,
     `phoneNumber` varchar(20) NOT NULL DEFAULT '',
     `startDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `permissionName` varchar(50) NOT NULL,
-    `permissionId` varchar(50) NOT NULL,
-    PRIMARY KEY (`id`)
+    `permission` varchar(50) NOT NULL COMMENT 'manager-quản lý\r\nstaff-nhân viên\r\ninactive-nghỉ việc',
+    `salary` int NOT NULL,
+    PRIMARY KEY (`employeeId`)
 );
-CREATE TABLE IF NOT EXISTS `Food_Category` (
-    `id` int NOT NULL AUTO_INCREMENT,
+
+CREATE TABLE IF NOT EXISTS `food_category` (
+    `foodCategoryId` int NOT NULL AUTO_INCREMENT,
     `name` varchar(50) UNIQUE NOT NULL,
     `slug` varchar(50) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`foodCategoryId`)
 );
-CREATE TABLE IF NOT EXISTS `Food_Item` (
-    `id` int NOT NULL AUTO_INCREMENT AUTO_INCREMENT=1,
+
+CREATE TABLE IF NOT EXISTS `food_item` (
+    `foodItemId` int NOT NULL AUTO_INCREMENT,
     `name` varchar(50) UNIQUE NOT NULL,
     `description` varchar(500) NULL,
-    `urlImage` varchar(50) NULL,
+    `imagePath` varchar(50) NULL,
     `unitName` varchar(20) NOT NULL,
     `unitPrice` bigint NOT NULL,
-    `idCategory` int NOT NULL,
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_item_category` FOREIGN KEY (`idCategory`) REFERENCES `Food_Category` (`id`)
+    `foodCategoryId` int NOT NULL,
+    PRIMARY KEY (`foodItemId`),
+    FOREIGN KEY (`foodCategoryId`) REFERENCES `food_category` (`foodCategoryId`)
 );
-CREATE TABLE IF NOT EXISTS `Order` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `idEmployee` int NOT NULL,
-    `idTable` int NULL,
+
+CREATE TABLE IF NOT EXISTS `order` (
+    `orderId` int NOT NULL AUTO_INCREMENT,
+    `employeeId` int NOT NULL,
+    `tableId` int NULL,
+    `customerId` int NULL,
     `type` varchar(45) NOT NULL DEFAULT 'local' COMMENT 'local - tại quán\nonline - đặt online',
     `status` varchar(45) NOT NULL DEFAULT 'unpaid' COMMENT 'unpaid - chưa thanh toán\npaid - đã thanh toán',
     `orderDate` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `payDate` TIMESTAMP NULL,
     `paidAmount` bigint NULL DEFAULT 0,
+    `rebate` bigint NOT NULL DEFAULT 0,
     `totalAmount` bigint NOT NULL DEFAULT 0,
     `discount` int NOT NULL DEFAULT 0,
-    CHECK(discount >= 0 AND discount <= 100)
-    PRIMARY KEY (`id`),
-    CONSTRAINT `fk_employee_order` FOREIGN KEY (`idEmployee`) REFERENCES `Employee` (`id`),
-    CONSTRAINT `fk_order_table` FOREIGN KEY (`idTable`) REFERENCES `Table` (`id`)
+    CHECK(discount >= 0 AND discount <= 100),
+    PRIMARY KEY (`orderId`),
+    FOREIGN KEY (`employeeId`) REFERENCES `employee` (`employeeId`),
+    FOREIGN KEY (`tableId`) REFERENCES `table` (`tableId`),
+    FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`)
 );
-CREATE TABLE IF NOT EXISTS `Shipment` (
-    `idOrder` int NOT NULL,
-    `idCustomer` int NOT NULL,
-    `shipperName` varchar(50) NOT NULL,
-    `shipperPhoneNumber` varchar(20) NOT NULL,
+
+CREATE TABLE IF NOT EXISTS `shipment` (
+    `orderId` int NOT NULL,
+    `customerId` int NOT NULL,
+    `employeeId` int NOT NULL,
     `status` varchar(45) NOT NULL DEFAULT 'topay' COMMENT 'topay - chờ xác nhận\ntoship - chờ lấy hàng\ntoreceive - đang giao\ncompleted - hoàn thành\ncancelled - đã hủy',
-    `notice` varchar(45) NULL,
     `startDate` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     `endDate` TIMESTAMP NULL,
-    PRIMARY KEY (`idOrder`),
-    CONSTRAINT `fk_ship_customer` FOREIGN KEY (`idCustomer`) REFERENCES `Customer` (`id`),
-    CONSTRAINT `fk_order_ship` FOREIGN KEY (`idOrder`) REFERENCES `Order` (`id`)
+    PRIMARY KEY (`orderId`),
+    FOREIGN KEY (`customerId`) REFERENCES `customer` (`customerId`),
+    FOREIGN KEY (`orderId`) REFERENCES `order` (`orderId`),
+    FOREIGN KEY (`employeeId`) REFERENCES `employee` (`employeeId`)
 );
-CREATE TABLE IF NOT EXISTS `Order_Item` (
-    `idOrder` int NOT NULL,
-    `idFoodItem` int NOT NULL,
-    `idTopping` int NOT NULL DEFAULT 1,
+
+CREATE TABLE IF NOT EXISTS `order_item` (
+    `orderId` int NOT NULL,
+    `foodItemId` int NOT NULL,
+    `toppingId` int NOT NULL DEFAULT 1,
     `quantity` int NOT NULL DEFAULT 1,
     `foodPrice` bigint(20) NOT NULL DEFAULT 0,
     `toppingPrice` bigint(20) NOT NULL DEFAULT 0,
     `note` varchar(100) NULL,
-    PRIMARY KEY ( `idOrder`, `idFoodItem`, `idTopping`),
-    CONSTRAINT `fk_order_main_item` FOREIGN KEY (`idFoodItem`) REFERENCES `Food_Item` (`id`),
-    CONSTRAINT `fk_order_topping` FOREIGN KEY (`idTopping`) REFERENCES `Food_Item` (`id`),
-    CONSTRAINT `fk_order_item` FOREIGN KEY (`idOrder`) REFERENCES `Order` (`id`)
+    PRIMARY KEY (`orderId`, `foodItemId`, `toppingId`),
+    FOREIGN KEY (`foodItemId`) REFERENCES `food_item` (`foodItemId`),
+    FOREIGN KEY (`toppingId`) REFERENCES `food_item` (`foodItemId`),
+    FOREIGN KEY (`orderId`) REFERENCES `order` (`orderId`)
+);
+
+CREATE TABLE IF NOT EXISTS `topping` (
+    `toppingId` int NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) UNIQUE NOT NULL,
+    `price` bigint NOT NULL,
+    PRIMARY KEY (`toppingId`)
+);
+
+CREATE TABLE IF NOT EXISTS `session` (
+    `sessionId` int NOT NULL AUTO_INCREMENT,
+    `employeeId` int NOT NULL,
+    `startTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `endTime` TIMESTAMP NULL DEFAULT NULL,
+    `message` varchar(200) DEFAULT NULL,
+    PRIMARY KEY (`sessionId`),
+    FOREIGN KEY (`employeeId`) REFERENCES `employee` (`employeeId`)
 );
